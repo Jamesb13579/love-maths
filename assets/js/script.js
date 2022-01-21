fdf42eed4a459e9d02459a85518c8e612de9566b
Jamesb13579 <jamesbagge@ymail.com>
2022-01-21 22:45:56 +0000

    add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,8 @@ function runGame(gameType){
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division"){
-        displayDivisionQuestion(num1, num2);
+        let newNum = Math.floor(Math.random()*15)+1
+        displayDivisionQuestion(num1 * newNum, num1);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting`;
@@ -142,7 +143,7 @@ function displayMultiplyQuestion(operand1, operand2){
 
 function displayDivisionQuestion(operand1, operand2){
 
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "/";
 }
